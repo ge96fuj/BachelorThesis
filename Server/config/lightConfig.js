2efd4ae2a6f3872a8f3ec5640eb2937555e90e47
Skovich <83189155+ge96fuj@users.noreply.github.com>
2025-05-04 22:02:02 +0200

    update

---
+++ Server/config/lightConfig.js
@@ -0,0 +1,18 @@
+module.exports = [
+ 
+  {
+    id: "tl_1",
+    localization_x: 0,
+    localization_y: 0,
+    group: "C",
+    durations: { red: 2000, yellow: 2000, green: 4000 }
+  },
+  {
+    id: "tl_2",
+    localization_x: 0,
+    localization_y: 0,
+    group: "C",
+    durations: { red: 2000, yellow: 2000, green: 20000 }
+  }
+  
+];
